77d6605f0009a9364c8b311460e1356b8d08d1c3
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-08-27 13:51:47 -0700

    Update APPX visual assets from new icons

---
+++ Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/ScratchLink_autogen/EWIEGA46WW/qrc_res.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/ScratchLink_autogen/EWIEGA46WW/qrc_res.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/ScratchLink_autogen/mocs_compilation.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/ScratchLink_autogen/mocs_compilation.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/controller/ScratchLinkController.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/controller/ScratchLinkController.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/main.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/main.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/ScratchLinkApplication.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/ScratchLinkApplication.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/ScratchLinkBluetoothClient.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/ScratchLinkBluetoothClient.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/ScratchLinkProtocolVersion.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/ScratchLinkProtocolVersion.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/ScratchLinkWebSocketServer.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/ScratchLinkWebSocketServer.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/json-rpc/JSONRPCMessage.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/json-rpc/JSONRPCMessage.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/json-rpc/JSONRPCNotification.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/json-rpc/JSONRPCNotification.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/json-rpc/JSONRPCRequest.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/json-rpc/JSONRPCRequest.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/model/json-rpc/JSONRPCResponse.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/model/json-rpc/JSONRPCResponse.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/view/ScratchLinkTrayIcon.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/view/ScratchLinkTrayIcon.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/view/ScratchLinkTrayIconMenu.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/view/ScratchLinkTrayIconMenu.cpp.o"
+  "/home/ascor8522/Documents/scratch-link/Linux/src/view/ScratchLinkView.cpp" "/home/ascor8522/Documents/scratch-link/Linux/cmake-build-debug/CMakeFiles/ScratchLink.dir/src/view/ScratchLinkView.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_WEBSOCKETS_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "ScratchLink_autogen/include"
+  "generated"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include/QtWidgets"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include/QtGui"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include/QtCore"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/./mkspecs/linux-g++"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include/QtWebSockets"
+  "/home/ascor8522/Qt/5.12.2/gcc_64/include/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
